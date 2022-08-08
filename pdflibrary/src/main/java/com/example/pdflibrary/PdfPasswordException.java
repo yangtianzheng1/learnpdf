aa8342518848aac99ca5d2b10fc6a47ceeb782f8
yang <yangtianzheng1@gmail.com>
2022-08-08 19:58:51 +0800

    init

---
+++ pdflibrary/src/main/java/com/example/pdflibrary/PdfPasswordException.java
@@ -0,0 +1,10 @@
+package com.example.pdflibrary;
+
+public class PdfPasswordException extends RuntimeException {
+    public PdfPasswordException() {
+    }
+
+    public PdfPasswordException(String message) {
+        super(message);
+    }
+}
